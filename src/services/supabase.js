d801da201e7f29868da17f915c092511637bb106
Zahidul-Turja <zahidulturja@gmail.com>
2024-02-15 11:32:26 +0600

    ENV variables added

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://zsohrkvvrvnxwxbhedoj.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpzb2hya3Z2cnZueHd4YmhlZG9qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc5MzMyOTgsImV4cCI6MjAyMzUwOTI5OH0.LaP-jTnBlydjGyo5vk4pdnKJpONPJf_rKR4xTtpqDvo";
+const supabaseKey = import.meta.env.VITE_supabaseKey;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
