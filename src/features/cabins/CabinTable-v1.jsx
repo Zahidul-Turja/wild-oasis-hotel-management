8839f07f1677253fd56044c91de60a8c97973e5b
Zahidul-Turja <zahidulturja@gmail.com>
2024-02-18 21:04:58 +0600

    Settings page initial func done

--- src/features/cabins/CabinTable-v1.jsx
+++ src/features/cabins/CabinTable-v1.jsx
@@ -1,8 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
+import styled from "styled-components";
 
 import { getCabins } from "../../services/apiCabins";
 import Spinner from "../../ui/Spinner";
-import styled from "styled-components";
 import CabinRow from "./CabinRow";
 
 const Table = styled.div`
