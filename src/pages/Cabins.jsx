3d51f927370822fee6032063ff6b02426e9660f8
Zahidul-Turja <zahidulturja@gmail.com>
2024-02-28 20:31:12 +0600

    reuseable filter functionality added

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,17 +1,15 @@
+import CabinTable from "../features/cabins/CabinTable";
+import AddCabin from "../features/cabins/AddCabin";
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import CabinTable from "../features/cabins/CabinTable";
-import Button from "../ui/Button";
-import { useState } from "react";
-import CreateCabinForm from "../features/cabins/CreateCabinForm";
-import AddCabin from "../features/cabins/AddCabin";
 
 function Cabins() {
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>Filter / sort</p>
+        <CabinTableOperations />
       </Row>
 
       <Row>
