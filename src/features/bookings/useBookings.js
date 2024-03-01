1608df03e6eb805fdc60a21a5759446518df17e0
Zahidul-Turja <zahidulturja@gmail.com>
2024-03-01 15:44:35 +0600

    booking sort done

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -5,20 +5,25 @@ import { useSearchParams } from "react-router-dom";
 export function useBookings() {
   const [searchParams] = useSearchParams();
 
-  // !Filter
+  // ! Filter
   const filterValue = searchParams.get("status");
   const filter =
     !filterValue || filterValue === "all"
       ? null
       : { field: "status", value: filterValue };
 
+  // ! Sort
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter], //? It's like the dependency array of useEffect
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy], //? It's like the dependency array of useEffect
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, error, bookings };
